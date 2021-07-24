9a675de20187041a77a0359d299b0c4e3e49941f
Armando Prado <armap1999@hotmail.com>
2021-07-24 10:29:13 -0500

    Update web.php
    Ways to set paths for the web page.

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,29 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+//Rutas de nuetra pagina
 
 Route::get('/', function () {
-    return view('welcome');
+    //return "Hola desde la pagina de inicio";
+    echo "<a href='" . route('contactos') . "'>Contactos 1<br>";
+    echo "<a href='" . route('contactos') . "'>Contactos 2<br>";
+    echo "<a href='" . route('contactos') . "'>Contactos 3<br>";
+    echo "<a href='" . route('contactos') . "'>Contactos 4<br>";
+    echo "<a href='" . route('contactos') . "'>Contactos 5<br>";
 });
+
+Route::get('contacto', function () {
+    return "vista de la pagina de contacto";
+});
+
+Route::get('saludo/{nombre}', function ($nombre) {
+    return "Saludos " . $nombre;
+});
+
+Route::get('saludos/{nombre?}', function ($nombre = "Incitado") { //? sirve para que no sea obligatorio el parametro
+    return "Saludos " . $nombre;
+});
+
+//Rutas con nombre o Named Routes
+Route::get('contactame', function () {
+    return "vista de la pagina de contactos";
+})->name('contactos');
